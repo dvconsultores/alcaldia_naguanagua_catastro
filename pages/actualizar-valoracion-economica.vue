c7da49efc282a4cdd00f765e349648590faa226d
sebastian3500 <sebastianrosa39@gmail.com>
2023-06-27 17:38:43 -0400

    commit

--- pages/actualizar-valoracion-economica.vue
+++ pages/actualizar-valoracion-economica.vue
@@ -18,6 +18,9 @@
               <v-autocomplete
               class="input-small outlined"
               label="Tipología"
+              :items="dataTipologia"
+              item-text="descripcion"
+              item-value="id"
               ></v-autocomplete>  
             </v-col>
 
@@ -25,6 +28,9 @@
               <v-autocomplete
               class="input-small"
               label="Fines Fiscales"
+              :items="dataFinesFiscales"
+              item-text="descripcion"
+              item-value="id"
               ></v-autocomplete>  
 
               <v-text-field
@@ -47,6 +53,9 @@
                 <v-autocomplete
                 class="input-date"
                 label="Forma"
+                :items="dataForma"
+                item-text="descripcion"
+                item-value="id"
                 ></v-autocomplete>
               </v-card>
             </v-col>
@@ -71,38 +80,44 @@
           <p class="actualizar-valoracion-economica-title">
             Datos de la Construcción
           </p>
+          <v-icon class="bold" color="#fff" x-large @click="dialog_crear = true">mdi-plus</v-icon>
         </div>
         <div class="div-card pt-8 wrap" style="flex-direction: column;">
-          <v-row>
+          <v-row v-for="(item, index) in dataConstruccion" :key="index" style="border-bottom: 2px solid var(--primary);">
             <v-col lg="7" class="divrow pr-0">
               <v-text-field
               class="input-small outlined"
               label="Tipología"
               readonly
+              :value="item.tipologia"
               ></v-text-field>
 
               <v-text-field
               class="input-small outlined"
               label="Fecha Compra"
               readonly
+              :value="item.fecha_compra"
               ></v-text-field>
 
               <v-text-field
               class="input-small outlined"
               label="Trimestre"
               readonly
+              :value="item.trimestre"
               ></v-text-field>
 
               <v-text-field
               class="input-small outlined"
               label="Año"
               readonly
+              :value="item.anio"
               ></v-text-field>
 
               <v-text-field
               class="input-small outlined"
               label="Área (m2)"
               readonly
+              :value="item.area"
               ></v-text-field> 
             </v-col>
 
@@ -111,12 +126,14 @@
               class="input-small"
               label="Valor Bs/m2"
               readonly
+              :value="item.valor"
               ></v-text-field> 
 
               <v-text-field
               class="input-small"
               label="Depreciación %"
               readonly
+              :value="item.depreciacion"
               ></v-text-field> 
 
               <v-text-field
@@ -125,15 +142,13 @@
               readonly
               ></v-text-field> 
 
-              <v-btn class="btn-delete">
+              <v-btn class="btn-delete" @click="deleteItem(index)">
                 <v-icon>
                   mdi-delete
                 </v-icon>
               </v-btn>
             </v-col>
-          </v-row>
 
-          <v-row>
             <v-col md="2">
               <v-text-field
               class="input-small outlined"
@@ -156,9 +171,7 @@
             </v-col>
           </v-row>
           
-          <hr>
-
-          <v-row class="mt-3">
+          <!-- <v-row class="mt-3">
             <v-col lg="5" class="divrow aend pr-0">
               <v-autocomplete
               class="input-small outlined"
@@ -177,14 +190,14 @@
 
               <v-autocomplete
               class="input-small"
-              label="Tipología"
+              label="% Depreciacion"
               ></v-autocomplete> 
             </v-col>
 
             <v-col lg="3" class="pl-1 pr-1">
               <v-card class="border-card-title-top">
                 <span class="span-float">
-                  Fecha Habitabilidad  <sup class="bold">*</sup>
+                  Fecha de Compra  <sup class="bold">*</sup>
                 </span>
 
                 <v-autocomplete
@@ -205,15 +218,15 @@
             </v-col>
 
             <v-col lg="4" class="divrow aend pl-1">
-              <v-text-field
-              class="input-small"
+              <v-autocomplete
+              class="input-small outlined"
               label="Trimestre"
-              ></v-text-field>
+              ></v-autocomplete>
 
-              <v-text-field
-              class="input-small"
+              <v-autocomplete
+              class="input-small outlined"
               label="Año"
-              ></v-text-field>
+              ></v-autocomplete>
 
               <v-btn class="btn-delete">
                 <v-icon>
@@ -221,7 +234,7 @@
                 </v-icon>
               </v-btn>
             </v-col>
-          </v-row>
+          </v-row> -->
         </div>
       </div>
 
@@ -271,19 +284,152 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
+
+    <v-dialog content-class="dialog-agregar-construccion" max-width="1600px" v-model="dialog_crear">
+      <v-card class="card-crear">
+        <v-card-title>
+          <span class="title">Agregar Construcción</span>
+        </v-card-title>
+
+        <hr>
+
+        <v-card-text>
+          <v-container>
+            <v-row class="center">
+              <v-col lg="12" class="divrow pr-0">
+              <v-autocomplete
+              class="input-small"
+              label="Tipología"
+              v-model="tipologia"
+              :items="dataTipologia"
+              item-text="descripcion"
+              item-value="id"
+              ></v-autocomplete>
+
+              <v-menu
+              v-model="menu_fecha"
+              :close-on-content-click="false"
+              :nudge-right="5"
+              transition="scale-transition"
+              offset-y
+              min-width="auto"
+              >
+                <template #activator="{ on, attrs }">
+                  <v-text-field
+                  v-model="fecha_compra"
+                  class="input-small"
+                  label="Fecha Compra"
+                  append-icon="mdi-calendar"
+                  readonly
+                  v-bind="attrs"
+                  v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="selectedDate"
+                  label="Fecha Compra"
+                  @input="formatoFecha()"
+                  color="#ff4081"
+                  header-color="#810880"
+                  class="custom-date-picker"
+                ></v-date-picker>
+              </v-menu>
+            </v-col>
+
+            <v-col lg="12" class="divrow pr-0">
+              <v-text-field
+              class="input-small"
+              label="Trimestre"
+              v-model="trimestre"
+              ></v-text-field>
+
+              <v-text-field
+              class="input-small"
+              label="Año"
+              v-model="anio"
+              ></v-text-field>
+            </v-col>
+
+            <v-col lg="12" class="divrow pr-0">
+              <v-text-field
+              class="input-small"
+              label="Área (m2)"
+              v-model="area"
+              ></v-text-field> 
+
+              <v-text-field
+              class="input-small"
+              label="Valor Bs/m2"
+              v-model="valor"
+              ></v-text-field> 
+            </v-col>
+
+            <v-col lg="12" class="divrow pr-0">
+              <v-text-field
+              class="input-small"
+              label="Depreciación %"
+              v-model="depreciacion"
+              ></v-text-field> 
+
+              <v-text-field
+              class="input-small"
+              label="Valor Actual Bs"
+              ></v-text-field> 
+            </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions class=center>
+          <v-btn
+            class="btn dialog-btn"
+            @click="dialog_crear = false"
+          >
+            Cancelar
+          </v-btn>
+
+          <v-btn
+            class="btn dialog-btn"
+            @click="addDiv()"
+            style="background-color:#ED057E!important;"
+          >
+            Guardar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script>
 import computeds from '~/mixins/computeds'
+import moment from 'moment'
 
 export default {
   name: "actualizar-valoracion-economicaPage",
   mixins: [computeds],
   data() {
     return {  
+      menu_fecha: false,
+      dialog_crear: false,
       dialog_confirmar: false,
       show_observaciones: false,
+      dataConstruccion:[
+        {
+          tipologia:"",
+          fecha_compra:"",
+          trimestre:"",
+          anio:"",
+          area:"",
+          valor:"",
+          depreciacion:"",
+          observaciones:"",
+        },
+      ],
+
+      selectedDate: null,
+      dataTipologia:[],
+      dataFinesFiscales:[],
+      dataForma:[],
     }
   },
   head() {
@@ -294,11 +440,60 @@ export default {
   },
 
   mounted(){
-
+    this.getTipologia()
+    this.getFinesFiscales()
+    this.getForma()
   },
 
   methods: {
-    
+    getTipologia(){
+      this.$axios.$get('tipologia').then(response => {
+        this.dataTipologia = response
+      }).catch(err => {
+        console.log(err)
+      })
+    },
+
+    getFinesFiscales(){
+      this.$axios.$get('finesfiscales').then(response => {
+        this.dataFinesFiscales = response
+      }).catch(err => {
+        console.log(err)
+      })
+    },
+
+    getForma(){
+      this.$axios.$get('forma').then(response => {
+        this.dataForma = response
+      }).catch(err => {
+        console.log(err)
+      })
+    },
+
+    addDiv(){
+      this.dataConstruccion.push({
+          tipologia: this.tipologia,
+          fecha_compra: this.fecha_compra,
+          trimestre: this.trimestre,
+          anio: this.anio,
+          area: this.area,
+          valor: this.area,
+          depreciacion: this.depreciacion,
+      })
+
+      this.dialog_crear = false
+    },  
+
+    deleteItem(index){
+      this.dataConstruccion.splice(index, 1)
+    },
+
+    formatoFecha() {
+      if (this.selectedDate) {
+        const formatted = moment(this.selectedDate).format('YYYY-MM-DD HH:mm:ss');
+        this.fecha_compra = formatted;
+      }
+    },
   }
 };
 </script>
